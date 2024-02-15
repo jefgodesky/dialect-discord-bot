d974cd0e74dbc312bf474d83c24c83c75e355133
Jason Godesky <jefgodesky@gmail.com>
2024-02-14 20:41:19 -0500

    Size deck to type

--- deck/deck.py
+++ deck/deck.py
@@ -1,3 +1,10 @@
+from typing import Literal
+
+DeckType = Literal["voice", "age1", "age2", "age3", "legacy"]
+
+
 class Deck:
-    def __init__(self, deck_type: str = "voice"):
+    def __init__(self, deck_type: DeckType = "voice"):
+        sizes = {"voice": 15, "age1": 22, "age2": 25, "age3": 14, "legacy": 6}
         self.deck_type = deck_type
+        self.cards = [x + 1 for x in range(sizes[deck_type])]
--- deck/tests/test_deck.py
+++ deck/tests/test_deck.py
@@ -25,3 +25,23 @@ class TestDeck:
     def test_creates_age3_deck(self):
         deck = Deck("age3")
         assert deck.deck_type == "age3"
+
+    def test_voice_has_15_cards(self):
+        deck = Deck("voice")
+        assert len(deck.cards) == 15
+
+    def test_legacy_has_6_cards(self):
+        deck = Deck("legacy")
+        assert len(deck.cards) == 6
+
+    def test_age1_has_22_cards(self):
+        deck = Deck("age1")
+        assert len(deck.cards) == 22
+
+    def test_age2_has_25_cards(self):
+        deck = Deck("age2")
+        assert len(deck.cards) == 25
+
+    def test_age3_has_14_cards(self):
+        deck = Deck("age3")
+        assert len(deck.cards) == 14
